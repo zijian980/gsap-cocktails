f800f45cc45d175df10ae1ad63b2d1cd4e787218
Zijian <163723805+zijian980@users.noreply.github.com>
2025-06-22 20:55:38 +0800

    Implement Hero Scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,6 @@ const App = () => {
         <main>
             <Navbar />
             <Hero />
-            <div className="h-dvh bg-black">
-
-            </div>
         </main>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import {useGSAP} from "@gsap/react";
 import {SplitText} from "gsap/all";
 import gsap from "gsap";
+import { useRef } from "react";
+import {useMediaQuery} from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth: 767});
+
     useGSAP(()=>{
         const heroSplit = new SplitText('.title',{ type: 'chars, words'});
         const paragraphSplit = new SplitText('.subtitle',{ type: 'lines'});
@@ -35,6 +41,24 @@ const Hero = () => {
         })
             .to(".right-leaf", { y: 200},0)
             .to(".left-leaf", { y: 200},0)
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
     },[]);
 
     return(
@@ -64,6 +88,16 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+            </div>
         </>
     )
 }
