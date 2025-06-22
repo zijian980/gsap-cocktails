5ad701c2954571f1ccbc7094dfdbb8a839eb2f7a
Zijian <163723805+zijian980@users.noreply.github.com>
2025-06-22 21:33:24 +0800

    Implement Cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,6 +11,7 @@ const App = () => {
         <main>
             <Navbar />
             <Hero />
+            <Cocktails />
         </main>
     )
 }
---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,66 @@
+import {cocktailLists, mockTailLists} from "../../constants/index.js";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+const Cocktails = () => {
+    useGSAP(() =>{
+        const parallaxTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#cocktails",
+                start: "top 30%",
+                end: "bottom 50%",
+                scrub: true,
+            }
+        })
+        parallaxTimeline
+            .from('#c-left-leaf', {
+                x: -100, y: 100
+            })
+            .from('#c-right-leaf', {
+                x: 100, y: 100
+            })
+    })
+
+    return (
+        <section id="cocktails" className="noisy">
+            <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf"/>
+            <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf"/>
+
+            <div className="list">
+                <div className="popular">
+                    <h2>Most Popular Cocktails:</h2>
+
+                    <ul>
+                        {cocktailLists.map(({name, country, detail, price}) => (
+                            <li key={name}>
+                                <div className="md:me-28">
+                                    <h3>{name}</h3>
+                                    <p3>{country} | {detail}</p3>
+                                </div>
+                                <span>- {price}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="loved">
+                    <h2>Most loved mocktails:</h2>
+
+                    <ul>
+                        {mockTailLists.map(({name, country, detail, price}) => (
+                            <li key={name}>
+                                <div className="me-28">
+                                    <h3>{name}</h3>
+                                    <p3>{country} | {detail}</p3>
+                                </div>
+                                <span>- {price}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
+        </section>
+    );
+}
+export default Cocktails
