ea1504a3186cf671abeb0497cf8dceaffc33cb6b
Zijian <163723805+zijian980@users.noreply.github.com>
2025-06-23 01:30:37 +0800

    Implemented Menu section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ const App = () => {
             <Cocktails />
             <About />
             <Art />
+            <Menu />
         </main>
     )
 }
